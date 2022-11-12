4d98f73bb346d4f6bb6c601bece5bfb870bd9084
anaahmedgohary <gogoahmed13@gmail.com>
2022-11-12 16:56:01 +0200

    added depends

--- src/api.js
+++ src/api.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+
+
+require("dotenv").config();
+const db = mysql.createConnection(process.env.DATABASE_URL);
+db.connect((err) =>
+{
+    if (err) throw err;
+    console.log("Database Connected");
+})
 
 const app = express();
 
@@ -12,7 +24,51 @@ router.get('/', (req, res) =>
     })
 })
 
-app.use('/.netlify/functions/api', router)
+router
+    .post("/portfoliomessage", (req, res) =>
+    {
+        let sqlcommand = "INSERT INTO portfoliomsg SET ?";
+        let body = req.body;
+       // let message = body.message;
+        let { name, email, message } = body;
+        let post = {
+            name: name,
+            email: email,
+            message: message
+        };
+        db.query(sqlcommand, post, (err, result) =>
+        {
+            if (err) { throw err; };
+            console.log(result);
+            res.send("Message Was Sent Successfully.");
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app
+    .use('/.netlify/functions/api', router)
+    .use(cors())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
 
 module.exports = app;
 module.exports.handler = serverless(app);
